a85ae65d935583c21ffdab94310e1a98f5fc0409
CarlosHenryWilliams <carloshenrywilliams@gmail.com>
2025-11-26 14:14:40 -0300

    Lista de clientes y ventas en sidenav

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { ProductGrid } from './pages/product-grid/product-grid';
 
 export const routes: Routes = [
-    {path: 'lista-productos', component:ProductGrid},
+    {path: '', component:ProductGrid},
     
     // 404 NOT FOUND
 ]
